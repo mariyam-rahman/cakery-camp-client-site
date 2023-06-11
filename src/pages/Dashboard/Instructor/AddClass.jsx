17c08e3c0cef4a885f224abcb5781026e8306f58
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-06-11 11:46:46 +0600

    my classes section added in instructor panel

--- src/pages/Dashboard/Instructor/AddClass.jsx
+++ src/pages/Dashboard/Instructor/AddClass.jsx
@@ -1,5 +1,141 @@
 const AddClass = () => {
-  return <div>add class</div>;
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto lg:py-16 lg:px-6 ">
+        <div className="mx-auto text-center mb-8 lg:mb-16">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            My Selected Classes
+          </h2>
+          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+            Explore the whole collection of open-source web components and
+            elements built with the utility classes from Tailwind
+          </p>
+        </div>
+        <div className="">
+          <AddForm></AddForm>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const AddForm = () => {
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Add a new product
+        </h2>
+        <form action="#">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required=""
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="brand"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Brand
+              </label>
+              <input
+                type="text"
+                name="brand"
+                id="brand"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Product brand"
+                required=""
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="price"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="$2999"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="category"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Category
+              </label>
+              <select
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option selected="">Select category</option>
+                <option value="TV">TV/Monitors</option>
+                <option value="PC">PC</option>
+                <option value="GA">Gaming/Console</option>
+                <option value="PH">Phones</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="item-weight"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Item Weight (kg)
+              </label>
+              <input
+                type="number"
+                name="item-weight"
+                id="item-weight"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder={12}
+                required=""
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows={8}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your description here"
+                defaultValue={""}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+          >
+            Add product
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default AddClass;
--- src/pages/Dashboard/Instructor/MyClasses.jsx
+++ src/pages/Dashboard/Instructor/MyClasses.jsx
@@ -1,5 +1,67 @@
+import { Button, Table } from "flowbite-react";
+
 const MyClasses = () => {
-  return <div>MyClasses</div>;
+  return (
+    <div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto lg:py-16 lg:px-6 ">
+          <div className="mx-auto text-center mb-8 lg:mb-16">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              My Classes
+            </h2>
+            <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+              Explore the whole collection of open-source web components and
+              elements built with the utility classes from Tailwind
+            </p>
+          </div>
+          <div className="">
+            <ClassRow></ClassRow>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const ClassRow = () => {
+  return (
+    <Table striped className="text-center">
+      <Table.Head>
+        <Table.HeadCell>Id</Table.HeadCell>
+        <Table.HeadCell>Image</Table.HeadCell>
+        <Table.HeadCell>Class Name</Table.HeadCell>
+        <Table.HeadCell>Instructor</Table.HeadCell>
+        <Table.HeadCell>Email</Table.HeadCell>
+        <Table.HeadCell>Available seats</Table.HeadCell>
+        <Table.HeadCell>Total Enrolled</Table.HeadCell>
+        <Table.HeadCell>Price</Table.HeadCell>
+        <Table.HeadCell>Status</Table.HeadCell>
+        <Table.HeadCell>Feedback</Table.HeadCell>
+        <Table.HeadCell> Action</Table.HeadCell>
+      </Table.Head>
+      <Table.Body className="divide-y">
+        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          <Table.Cell>1</Table.Cell>
+          <Table.Cell>
+            {" "}
+            <img src="../../../assets/bread.jpg" alt="" />{" "}
+          </Table.Cell>
+
+          <Table.Cell>Red Velvet Cake</Table.Cell>
+          <Table.Cell>joe@gmail.com</Table.Cell>
+          <Table.Cell>Milky Donut</Table.Cell>
+          <Table.Cell>20</Table.Cell>
+          <Table.Cell>10</Table.Cell>
+          <Table.Cell>$100</Table.Cell>
+          <Table.Cell>Denied</Table.Cell>
+          <Table.Cell>improve more</Table.Cell>
+          <Table.Cell>
+            <Button>Update </Button>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
 };
 
 export default MyClasses;
